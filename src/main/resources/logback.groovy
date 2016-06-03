1f32af63a28ac4496751741005113f6bc1cb57b1
Hoang Tung Dinh <tunghdinh@gmail.com>
2016-06-04 00:07:06 +0200

    updated logback.groovy

--- src/main/resources/logback.groovy
+++ src/main/resources/logback.groovy
@@ -1,9 +1,3 @@
-import ch.qos.logback.classic.encoder.PatternLayoutEncoder
-import ch.qos.logback.core.ConsoleAppender
-import ch.qos.logback.core.FileAppender
-
-import static ch.qos.logback.classic.Level.INFO
-
 appender("STDOUT", ConsoleAppender) {
     encoder(PatternLayoutEncoder) {
         pattern = "%-4relative [%thread] %-5level %logger{35} - %msg %n"
