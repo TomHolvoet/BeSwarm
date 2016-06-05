d8a103acb278d412b589a50f4a15f5ee42ed083a
Hoang Tung Dinh <tunghdinh@gmail.com>
2016-06-05 16:08:51 +0200

    added circle trajectory

--- src/main/java/simulation/ExampleTrajectory.java
+++ src/main/java/simulation/ExampleTrajectory.java
@@ -9,15 +9,19 @@ import control.Trajectory4d;
  */
 public final class ExampleTrajectory implements Trajectory4d {
 
-    private final Trajectory1d trajectoryLinearX = SinglePointTrajectory1d.create(0, 0);
-    private final Trajectory1d trajectoryLinearZ = SinglePointTrajectory1d.create(1, 0);
+    private final Trajectory1d trajectoryLinearX = new CosineTrajectory();
+    private final Trajectory1d trajectoryLinearY = new SineTrajectory();
+    private final Trajectory1d trajectoryLinearZ = new SineTrajectory();
+
     private final Trajectory1d trajectoryAngularZ = SinglePointTrajectory1d.create(0, 0);
 
-    private final Trajectory1d trajectoryLinearY = SineTrajectory.create();
+    private double startTime = -1;
 
     private ExampleTrajectory() {}
 
-    public static ExampleTrajectory create() {return new ExampleTrajectory();}
+    public static ExampleTrajectory create() {
+        return new ExampleTrajectory();
+    }
 
     @Override
     public Trajectory1d getTrajectoryLinearX() {
@@ -39,14 +43,9 @@ public final class ExampleTrajectory implements Trajectory4d {
         return trajectoryAngularZ;
     }
 
-    private static final class SineTrajectory implements Trajectory1d {
-        private double startTime = -1;
+    private final class SineTrajectory implements Trajectory1d {
 
         private SineTrajectory() {}
-
-        public static SineTrajectory create() {
-            return new SineTrajectory();
-        }
 
         @Override
         public double getDesiredPosition(double timeInSeconds) {
@@ -55,7 +54,7 @@ public final class ExampleTrajectory implements Trajectory4d {
             }
 
             final double currentTime = timeInSeconds - startTime;
-            return StrictMath.sin(0.5 * currentTime);
+            return 2 * StrictMath.sin(0.25 * currentTime);
         }
 
         @Override
@@ -65,7 +64,34 @@ public final class ExampleTrajectory implements Trajectory4d {
             }
 
             final double currentTime = timeInSeconds - startTime;
-            return 0.5 * StrictMath.cos(0.5 * currentTime);
+            return 0.5 * StrictMath.cos(0.25 * currentTime);
+        }
+    }
+
+    private final class CosineTrajectory implements Trajectory1d {
+
+        private CosineTrajectory() {}
+
+        ;
+
+        @Override
+        public double getDesiredPosition(double timeInSeconds) {
+            if (startTime < 0) {
+                startTime = timeInSeconds;
+            }
+
+            final double currentTime = timeInSeconds - startTime;
+            return 2 * StrictMath.cos(0.25 * currentTime);
+        }
+
+        @Override
+        public double getDesiredVelocity(double timeInSeconds) {
+            if (startTime < 0) {
+                startTime = timeInSeconds;
+            }
+
+            final double currentTime = timeInSeconds - startTime;
+            return -0.5 * StrictMath.sin(0.25 * currentTime);
         }
     }
 }
