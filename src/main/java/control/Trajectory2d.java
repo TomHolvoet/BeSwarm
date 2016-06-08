aeb21aeb73a3b07550e7021cb1244f1ed6fc76dd
Kristof Coninx <kristof.coninx@cs.kuleuven.be>
2016-06-08 14:10:36 +0200

    Add documentation to Trajectory2d

--- src/main/java/control/Trajectory2d.java
+++ src/main/java/control/Trajectory2d.java
@@ -1,6 +1,10 @@
 package control;
 
 /**
+ * 2D Trajectory representing a trajectory in 2 planes of motion.
+ * Default plane of motion is the (x,y)-plane but the (x,z), and the (y,z) plane
+ * motion trajectories can also be modelled under this interface.
+ *
  * @author Kristof Coninx
  */
 public interface Trajectory2d {
