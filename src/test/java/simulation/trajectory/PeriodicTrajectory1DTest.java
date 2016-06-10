d9990ba81e16fb644786a1bfe8d41f9ce4549452
Kristof Coninx <kristof.coninx@cs.kuleuven.be>
2016-06-10 14:42:18 +0200

    Added other trajectory movement primitives: Pendulum, PendulumSwing and ConstantSwing. Refactored some commonalities for periodic movement to abstract super class.

---
+++ src/test/java/simulation/trajectory/PeriodicTrajectory1DTest.java
@@ -0,0 +1,84 @@
+package simulation.trajectory;
+
+import com.google.common.collect.Lists;
+import control.Trajectory1d;
+import control.Trajectory2d;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Collection;
+
+/**
+ * @author Kristof Coninx <kristof.coninx AT cs.kuleuven.be>
+ */
+@RunWith(value = Parameterized.class)
+public class PeriodicTrajectory1DTest {
+    private Trajectory1d highFrequencyCircle;
+    private Trajectory1d lowFrequencyCircle;
+    private final double lowFreq = 1 / 10;
+    private final double highFreq = 1.5;
+    private final double radius = 0.065;
+
+    public PeriodicTrajectory1DTest(Class cl) {
+        Class[] cArg = new Class[2];
+        cArg[0] = double.class;
+        cArg[1] = double.class;
+        try {
+            highFrequencyCircle = (Trajectory1d) cl.getDeclaredConstructor(cArg)
+                    .newInstance(radius, highFreq);
+            lowFrequencyCircle = (Trajectory1d) cl.getDeclaredConstructor(cArg)
+                    .newInstance(radius, lowFreq);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Parameterized.Parameters
+    public static Collection<? extends Class> getParams() {
+        return Lists.newArrayList(ConstantSwingTrajectory1D.class,
+                PendulumSwingTrajectory1D.class);
+    }
+
+    @Test
+    public void getTrajectoryLinearX() throws Exception {
+        for (double i = 0; i < 3;
+             i += 0.66) {
+            Assert.assertEquals(radius,
+                    highFrequencyCircle
+                            .getDesiredPosition(i), 0.01);
+        }
+
+        for (double i = 0; i < 30;
+             i += 10) {
+            Assert.assertEquals(radius,
+                    lowFrequencyCircle
+                            .getDesiredPosition(i), 0.01);
+        }
+
+        for (double i = 0; i < 30;
+             i += 2) {
+
+            Assert.assertTrue(
+                    Math.abs(highFrequencyCircle
+                            .getDesiredVelocity(i))
+                            < PeriodicTrajectory.MAX_ABSOLUTE_SPEED);
+        }
+
+        for (double i = 0; i < 30;
+             i += 2) {
+
+            Assert.assertTrue(
+                    Math.abs(lowFrequencyCircle
+                            .getDesiredVelocity(i))
+                            < PeriodicTrajectory.MAX_ABSOLUTE_SPEED);
+        }
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testConstructorWithHighThanAllowedSpeedRate() {
+        Trajectory2d target = new CircleTrajectory2D(5, 1, true);
+    }
+
+}
--- src/test/java/simulation/trajectory/PeriodicTrajectory2DTest.java
+++ src/test/java/simulation/trajectory/PeriodicTrajectory2DTest.java
@@ -1,25 +1,57 @@
 package simulation.trajectory;
 
+import com.google.common.collect.Lists;
 import control.Trajectory2d;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Collection;
 
 /**
- * Created by Kristof Coninx <kristof.coninx AT cs.kuleuven.be>
+ * @author Kristof Coninx <kristof.coninx AT cs.kuleuven.be>
  */
-public class CircleTrajectory2DTest {
+@RunWith(value = Parameterized.class)
+public class PeriodicTrajectory2DTest {
     private Trajectory2d highFrequencyCircle;
     private Trajectory2d lowFrequencyCircle;
     private final double lowFreq = 1 / 10;
     private final double highFreq = 1.5;
-    private final double radius = 0.1;
+    private final double radius = 0.065;
 
     @Before
     public void setUp() throws Exception {
-        highFrequencyCircle = new CircleTrajectory2D(radius, highFreq, false);
-        lowFrequencyCircle = new CircleTrajectory2D(radius, lowFreq, false);
+        //        highFrequencyCircle = new CircleTrajectory2D(radius,
+        // highFreq, false);
+        //        lowFrequencyCircle = new CircleTrajectory2D(radius,
+        // lowFreq, false);
 
+    }
+
+    public PeriodicTrajectory2DTest(Class cl) {
+        Class[] cArg = new Class[2];
+        cArg[0] = double.class;
+        cArg[1] = double.class;
+        try {
+            highFrequencyCircle = (Trajectory2d) cl.getDeclaredConstructor(cArg)
+                    .newInstance(radius, highFreq);
+            lowFrequencyCircle = (Trajectory2d) cl.getDeclaredConstructor(cArg)
+                    .newInstance(radius, lowFreq);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        //        highFrequencyCircle = new CircleTrajectory2D(radius, highFreq,
+        //                false);
+        //        lowFrequencyCircle = new CircleTrajectory2D(radius,
+        // lowFreq, false);
+    }
+
+    @Parameterized.Parameters
+    public static Collection<? extends Class> getParams() {
+        return Lists.newArrayList(PendulumTrajectory2D.class,
+                CircleTrajectory2D.class);
     }
 
     @Test
@@ -43,7 +75,8 @@ public class CircleTrajectory2DTest {
 
             Assert.assertTrue(
                     Math.abs(highFrequencyCircle.getTrajectoryLinearAbscissa()
-                            .getDesiredVelocity(i)) < 1);
+                            .getDesiredVelocity(i))
+                            < PeriodicTrajectory.MAX_ABSOLUTE_SPEED);
         }
 
         for (double i = 0; i < 30;
@@ -51,7 +84,8 @@ public class CircleTrajectory2DTest {
 
             Assert.assertTrue(
                     Math.abs(lowFrequencyCircle.getTrajectoryLinearAbscissa()
-                            .getDesiredVelocity(i)) < 1);
+                            .getDesiredVelocity(i))
+                            < PeriodicTrajectory.MAX_ABSOLUTE_SPEED);
         }
     }
 
@@ -76,7 +110,8 @@ public class CircleTrajectory2DTest {
 
             Assert.assertTrue(
                     Math.abs(highFrequencyCircle.getTrajectoryLinearOrdinate()
-                            .getDesiredVelocity(i)) < 1);
+                            .getDesiredVelocity(i))
+                            < PeriodicTrajectory.MAX_ABSOLUTE_SPEED);
         }
 
         for (double i = 0; i < 30;
@@ -84,7 +119,8 @@ public class CircleTrajectory2DTest {
 
             Assert.assertTrue(
                     Math.abs(lowFrequencyCircle.getTrajectoryLinearOrdinate()
-                            .getDesiredVelocity(i)) < 1);
+                            .getDesiredVelocity(i))
+                            < PeriodicTrajectory.MAX_ABSOLUTE_SPEED);
         }
     }
 
