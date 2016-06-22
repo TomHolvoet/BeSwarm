6776bf6c5bbab122fc0eef2a8698ce261c53acc9
Hoang Tung Dinh <tunghdinh@gmail.com>
2016-06-22 13:05:57 +0200

    added AscTecVelocityService

---
+++ src/main/java/services/asctec/AscTecVelocityService.java
@@ -0,0 +1,56 @@
+package services.asctec;
+
+import control.dto.Velocity;
+import hal_quadrotor.VelocityRequest;
+import hal_quadrotor.VelocityResponse;
+import org.ros.exception.RemoteException;
+import org.ros.node.service.ServiceClient;
+import org.ros.node.service.ServiceResponseListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import services.VelocityService;
+
+/**
+ * @author Hoang Tung Dinh
+ */
+public final class AscTecVelocityService implements VelocityService {
+    private static final Logger logger = LoggerFactory.getLogger(AscTecVelocityService.class);
+    private final ServiceClient<VelocityRequest, VelocityResponse> srvVelocity;
+
+    private AscTecVelocityService(ServiceClient<VelocityRequest, VelocityResponse> srvVelocity) {
+        this.srvVelocity = srvVelocity;
+    }
+
+    public static AscTecVelocityService create(ServiceClient<VelocityRequest, VelocityResponse> srvVelocity) {
+        return new AscTecVelocityService(srvVelocity);
+    }
+
+    @Override
+    public void sendVelocityMessage(Velocity velocity) {
+        final VelocityRequest velocityRequest = srvVelocity.newMessage();
+        velocityRequest.setDx(velocity.linearX());
+        velocityRequest.setDy(velocity.linearY());
+        velocityRequest.setDz(velocity.linearZ());
+        velocityRequest.setYaw(velocity.angularZ());
+        srvVelocity.call(velocityRequest, VelocityServiceResponseListener.create());
+    }
+
+    private static final class VelocityServiceResponseListener implements ServiceResponseListener<VelocityResponse> {
+        private VelocityServiceResponseListener() {}
+
+        public static VelocityServiceResponseListener create() {
+            return new VelocityServiceResponseListener();
+        }
+
+        @Override
+        public void onSuccess(VelocityResponse velocityResponse) {
+            logger.info("Successfully sent velocity message!!!");
+            logger.info(velocityResponse.getStatus());
+        }
+
+        @Override
+        public void onFailure(RemoteException e) {
+            logger.info("Cannot send velocity message!!!", e);
+        }
+    }
+}
