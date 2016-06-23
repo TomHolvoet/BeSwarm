30e1b5ebd20a6b1b73fa9ee92eccf13917deec04
Hoang Tung Dinh <tunghdinh@gmail.com>
2016-06-23 21:21:09 +0200

    added Pose to Velocity dto

--- src/main/java/control/dto/Velocity.java
+++ src/main/java/control/dto/Velocity.java
@@ -1,8 +1,9 @@
 package control.dto;
 
 import com.google.auto.value.AutoValue;
-
 import geometry_msgs.Twist;
+
+import javax.annotation.Nullable;
 
 /**
  * A value class which stores the velocity of the drone.
@@ -11,6 +12,8 @@ import geometry_msgs.Twist;
  */
 @AutoValue
 public abstract class Velocity {
+    protected Velocity() {}
+
     public abstract double linearX();
 
     public abstract double linearY();
@@ -19,29 +22,39 @@ public abstract class Velocity {
 
     public abstract double angularZ();
 
+    /**
+     * The pose of the drone associated with this velocity. This value is optional and is only for transforming
+     * velocity from global to local frame and vice versa.
+     *
+     * @return the pose of the drone associated with this velocity
+     */
+    @Nullable
+    public abstract Pose pose();
+
+    /**
+     * The {@link Builder#pose(Pose)} is optional.
+     *
+     * @return the builder
+     */
     public static Builder builder() {
-        return new AutoValue_Velocity.Builder();
+        return new AutoValue_Velocity.Builder().pose(null);
     }
 
     /**
      * Converts a Twist velocity (given in NED coordinates) to a local velocity using XYZ frame
      * TODO fix frame of reference for local velocity
-     * 
+     *
      * @return
      */
     public static Velocity createLocalVelocityFrom(Twist twist) {
-    	final double twistX = twist.getLinear().getX();
-    	final double twistY = twist.getLinear().getY();
-    	final double twistZ = twist.getLinear().getZ();
-    	final double twistAngularZ = twist.getAngular().getZ();
-    	
-    	return builder().linearX(twistX)
-    			.linearY(twistY)
-    			.linearZ(twistZ)
-    			.angularZ(twistAngularZ)
-    			.build();
+        final double twistX = twist.getLinear().getX();
+        final double twistY = twist.getLinear().getY();
+        final double twistZ = twist.getLinear().getZ();
+        final double twistAngularZ = twist.getAngular().getZ();
+
+        return builder().linearX(twistX).linearY(twistY).linearZ(twistZ).angularZ(twistAngularZ).build();
     }
-    
+
     @AutoValue.Builder
     public abstract static class Builder {
         public abstract Builder linearX(double value);
@@ -51,6 +64,8 @@ public abstract class Velocity {
         public abstract Builder linearZ(double value);
 
         public abstract Builder angularZ(double value);
+
+        public abstract Builder pose(@Nullable Pose pose);
 
         public abstract Velocity build();
     }
