643d7a749f3c9243e1c81b9771139f7bda3c7ce2
Hoang Tung Dinh <tunghdinh@gmail.com>
2016-06-23 11:41:29 +0200

    added velocity estimator for Crates simulator

---
+++ src/main/java/control/localization/CratesStateVelocityEstimator.java
@@ -0,0 +1,57 @@
+package control.localization;
+
+import com.google.common.base.Optional;
+import control.dto.Velocity;
+import hal_quadrotor.State;
+import services.ros_subscribers.MessagesSubscriberService;
+
+import java.util.Queue;
+
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkState;
+
+/**
+ * @author Hoang Tung Dinh
+ */
+public final class CratesStateVelocityEstimator implements VelocityEstimator {
+    private final MessagesSubscriberService<State> stateSubscriber;
+
+    private CratesStateVelocityEstimator(MessagesSubscriberService<State> stateSubscriber) {
+        checkArgument(stateSubscriber.getMaxMessageQueueSize() >= 2,
+                String.format("Max message queue size must be " + "at least 2, but it is %d.",
+                        stateSubscriber.getMaxMessageQueueSize()));
+        this.stateSubscriber = stateSubscriber;
+    }
+
+    public static CratesStateVelocityEstimator create(MessagesSubscriberService<State> stateSubscriber) {
+        return new CratesStateVelocityEstimator(stateSubscriber);
+    }
+
+    @Override
+    public Optional<Velocity> getCurrentVelocity() {
+        final Queue<State> messageQueue = stateSubscriber.getMessageQueue();
+        if (messageQueue.size() < 2) {
+            return Optional.absent();
+        } else {
+            final State firstState = messageQueue.poll();
+            final State secondState = messageQueue.poll();
+            final double timeDelta = firstState.getT() - secondState.getT();
+            checkState(timeDelta > 0, String.format("Time delta must be positive, but it is %f", timeDelta));
+            final double velocityX = get1dVelocity(firstState.getX() - secondState.getX(), timeDelta);
+            final double velocityY = get1dVelocity(firstState.getY() - secondState.getY(), timeDelta);
+            final double velocityZ = get1dVelocity(firstState.getZ() - secondState.getZ(), timeDelta);
+            final double velocityYaw = get1dVelocity(firstState.getYaw() - secondState.getYaw(), timeDelta);
+            final Velocity currentVelocity = Velocity.builder()
+                    .linearX(velocityX)
+                    .linearY(velocityY)
+                    .linearZ(velocityZ)
+                    .angularZ(velocityYaw)
+                    .build();
+            return Optional.of(currentVelocity);
+        }
+    }
+
+    private static double get1dVelocity(double travelledDistance, double timeDelta) {
+        return travelledDistance / timeDelta;
+    }
+}
--- src/main/java/services/ros_subscribers/MessagesSubscriberService.java
+++ src/main/java/services/ros_subscribers/MessagesSubscriberService.java
@@ -10,26 +10,33 @@ import org.ros.node.topic.Subscriber;
 import java.util.LinkedList;
 import java.util.Queue;
 
+import static com.google.common.base.Preconditions.checkArgument;
+
 /**
  * @author mhct
  */
 public class MessagesSubscriberService<T extends Message> {
     private final Subscriber<T> subscriber;
     private final MessagesListener<T> messagesListener;
+    private final int maxMessageQueueSize;
     private boolean startedListeningToMessages = false;
 
     private static final int DEFAULT_MESSAGE_QUEUE__SIZE = 1;
 
     protected MessagesSubscriberService(Subscriber<T> subscriber, int maxMessageQueueSize) {
+        checkArgument(maxMessageQueueSize >= 1,
+                String.format("Queue size must be at least 1, but it is %d.", maxMessageQueueSize));
         this.subscriber = subscriber;
         this.messagesListener = MessagesListener.create(maxMessageQueueSize);
+        this.maxMessageQueueSize = maxMessageQueueSize;
     }
 
     public static <Type extends Message> MessagesSubscriberService<Type> create(Subscriber<Type> subscriber) {
         return new MessagesSubscriberService<>(subscriber, DEFAULT_MESSAGE_QUEUE__SIZE);
     }
 
-    public static <Type extends Message> MessagesSubscriberService<Type> create(Subscriber<Type> subscriber, int maxMessageQueueSize) {
+    public static <Type extends Message> MessagesSubscriberService<Type> create(Subscriber<Type> subscriber,
+            int maxMessageQueueSize) {
         return new MessagesSubscriberService<>(subscriber, maxMessageQueueSize);
     }
 
@@ -46,6 +53,10 @@ public class MessagesSubscriberService<T extends Message> {
 
     public Queue<T> getMessageQueue() {
         return messagesListener.getMessageQueue();
+    }
+
+    public int getMaxMessageQueueSize() {
+        return maxMessageQueueSize;
     }
 
     private static final class MessagesListener<K extends Message> implements MessageListener<K> {
