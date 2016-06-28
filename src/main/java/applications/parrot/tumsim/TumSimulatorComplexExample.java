accaa5bffa47a1e319a70aff13cacfabdacfaa21
Kristof Coninx <kristof.coninx@cs.kuleuven.be>
2016-06-28 12:08:40 +0200

    Added initial hover to trajectory.

--- src/main/java/applications/parrot/tumsim/TumSimulatorComplexExample.java
+++ src/main/java/applications/parrot/tumsim/TumSimulatorComplexExample.java
@@ -27,6 +27,8 @@ public class TumSimulatorComplexExample extends AbstractNodeMain {
     }
 
     private Trajectory4d getConcreteTrajectory() {
+        Trajectory4d init = Trajectories
+                .newHoldPositionTrajectory(Point4D.create(0, 0, 1.5, 0));
         Trajectory4d first = Trajectories
                 .newStraightLineTrajectory(Point4D.create(0, 0, 1.5, 0),
                         Point4D.create(5, 5, 5, 0), 0.6);
@@ -35,7 +37,8 @@ public class TumSimulatorComplexExample extends AbstractNodeMain {
                         Math.PI / 4);
         Trajectory4d third = Trajectories
                 .newHoldPositionTrajectory(Point4D.create(1, 1, 2, 0));
-        return Choreography.builder().withTrajectory(first).forTime(20)
+        return Choreography.builder().withTrajectory(init).forTime(5)
+                .withTrajectory(first).forTime(20)
                 .withTrajectory(second).forTime(40).withTrajectory(third)
                 .forTime(30).build();
     }
