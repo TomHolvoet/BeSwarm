0d182b953ca67a928bbfb5d2f28c6a60c5d13854
Kristof Coninx <kristof.coninx@cs.kuleuven.be>
2016-06-30 15:18:01 +0200

    Refactoring example classes to use Finite Trajectories.

--- src/main/java/applications/parrot/tumsim/TumRunExampleTrajectory2.java
+++ src/main/java/applications/parrot/tumsim/TumRunExampleTrajectory2.java
@@ -1,13 +1,15 @@
 package applications.parrot.tumsim;
 
 import applications.ExampleTrajectory;
-import control.Trajectory4d;
+import choreo.Choreography;
+import control.FiniteTrajectory4d;
 import org.ros.namespace.GraphName;
 import org.ros.node.AbstractNodeMain;
 import org.ros.node.ConnectedNode;
 
 /**
- * This class is for running the simulation with the AR drone in the Tum simulator.
+ * This class is for running the simulation with the AR drone in the Tum
+ * simulator.
  *
  * @author Hoang Tung Dinh
  * @see <a href="https://github.com/dougvk/tum_simulator">The simulator</a>
@@ -21,8 +23,12 @@ public final class TumRunExampleTrajectory2 extends AbstractNodeMain {
 
     @Override
     public void onStart(final ConnectedNode connectedNode) {
-        final Trajectory4d trajectory = ExampleTrajectory.create();
-        final TumExampleFlightFacade flight = TumExampleFlightFacade.create(trajectory, connectedNode);
+        final double defaultTime = 60;
+        final FiniteTrajectory4d trajectory = Choreography.builder()
+                .withTrajectory(ExampleTrajectory.create())
+                .forTime(defaultTime).build();
+        final TumExampleFlightFacade flight = TumExampleFlightFacade
+                .create(trajectory, connectedNode);
         flight.fly();
     }
 }
