a5541c6d073b062e80070a5d3513b92a98b525ac
Kristof Coninx <kristof.coninx@cs.kuleuven.be>
2016-07-05 17:14:31 +0200

    tuned params.

--- src/main/java/applications/parrot/tumsim/TumSimulatorCircleExample.java
+++ src/main/java/applications/parrot/tumsim/TumSimulatorCircleExample.java
@@ -32,10 +32,10 @@ public class TumSimulatorCircleExample extends AbstractNodeMain {
                 .newHoldPositionTrajectory(Point4D.create(2, -2, 1.5, 0));
         Trajectory4d second = Trajectories
                 .newConstantYawCircleTrajectory4D(Point4D.create(1, -2, 1.5, 0),
-                        1, 0.05, 0);
+                        0.5, 0.05, 0);
         Trajectory4d third = Trajectories
-                .newHoldPositionTrajectory(Point4D.create(1, -2, 1, 0));
-        return Choreography.builder().withTrajectory(init).forTime(5).withTrajectory(second)
+                .newHoldPositionTrajectory(Point4D.create(2, -2, 1, 0));
+        return Choreography.builder().withTrajectory(init).forTime(10).withTrajectory(second)
                 .forTime(40).withTrajectory(third)
                 .forTime(10).build();
     }
