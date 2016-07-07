e4e0f0ac88695358fd1f0dd76d3627457a051ee6
Hoang Tung Dinh <tunghdinh@gmail.com>
2016-07-07 15:44:14 +0200

    changed the yaw in circle trajectory to -Pi/2

--- src/main/java/applications/parrot/tumsim/TumSimulatorCircleExample.java
+++ src/main/java/applications/parrot/tumsim/TumSimulatorCircleExample.java
@@ -22,23 +22,29 @@ public class TumSimulatorCircleExample extends AbstractNodeMain {
     @Override
     public void onStart(final ConnectedNode connectedNode) {
         final FiniteTrajectory4d trajectory = getConcreteTrajectory();
-        final TumExampleFlightFacade flight = TumExampleFlightFacade
-                .create(trajectory, connectedNode);
+        final TumExampleFlightFacade flight = TumExampleFlightFacade.create(trajectory, connectedNode);
         flight.fly();
     }
 
     public static FiniteTrajectory4d getConcreteTrajectory() {
         Trajectory4d init = Trajectories.newHoldPositionTrajectory(Point4D.create(2, -2, 1.5, 0));
-        Trajectory4d second = Trajectories
-                .newConstantYawCircleTrajectory4D(Point4D.create(1, -2, 1.5, 0), 0.5, 0.05, 0, 0);
+        Trajectory4d second = Trajectories.newConstantYawCircleTrajectory4D(Point4D.create(1, -2, 1.5, 0), 0.5, 0.05, 0,
+                0);
         //Alternatively, the builder api can also be used like this, to create circle trajectory.
         Trajectory4d alternativeSecond = Trajectories.CircleTrajectoryBuilder()
-                .setLocation(Point4D.create(1, -2, 1.5, 0)).setRadius(0.5).setFrequency(0.05)
-                .fixYawAt(0).build();
-        Trajectory4d third = Trajectories
-                .newHoldPositionTrajectory(Point4D.create(2, -2, 1, 0));
-        return Choreography.builder().withTrajectory(init).forTime(10).withTrajectory(second)
-                .forTime(40).withTrajectory(third)
-                .forTime(10).build();
+                .setLocation(Point4D.create(1, -2, 1.5, 0))
+                .setRadius(0.5)
+                .setFrequency(0.05)
+                .fixYawAt(-Math.PI / 2)
+                .build();
+        Trajectory4d third = Trajectories.newHoldPositionTrajectory(Point4D.create(2, -2, 1, 0));
+        return Choreography.builder()
+                .withTrajectory(init)
+                .forTime(10)
+                .withTrajectory(second)
+                .forTime(40)
+                .withTrajectory(third)
+                .forTime(10)
+                .build();
     }
 }
