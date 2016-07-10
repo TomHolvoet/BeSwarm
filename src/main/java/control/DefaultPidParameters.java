e7f044cc95abc412590b4be5d887ae13d55580d9
Hoang Tung Dinh <tunghdinh@gmail.com>
2016-07-10 10:24:27 +0200

    updated DefaultPidParameters

--- src/main/java/control/DefaultPidParameters.java
+++ src/main/java/control/DefaultPidParameters.java
@@ -7,28 +7,28 @@ public enum DefaultPidParameters {
     LINEAR_X {
         @Override
         public PidParameters getParameters() {
-            return PidParameters.builder().kp(0.3).kd(0.1).ki(0).lagTimeInSeconds(0.2).build();
+            return PidParameters.builder().kp(0.6).kd(0.3).ki(0).lagTimeInSeconds(0.2).build();
         }
     },
 
     LINEAR_Y {
         @Override
         public PidParameters getParameters() {
-            return PidParameters.builder().kp(0.3).kd(0.1).ki(0).lagTimeInSeconds(0.2).build();
+            return PidParameters.builder().kp(0.6).kd(0.3).ki(0).lagTimeInSeconds(0.2).build();
         }
     },
 
     LINEAR_Z {
         @Override
         public PidParameters getParameters() {
-            return PidParameters.builder().kp(0.3).kd(0.1).ki(0).lagTimeInSeconds(0.2).build();
+            return PidParameters.builder().kp(0.6).kd(0.3).ki(0).lagTimeInSeconds(0.2).build();
         }
     },
 
     ANGULAR_Z {
         @Override
         public PidParameters getParameters() {
-            return PidParameters.builder().kp(0.3).kd(0.1).ki(0).lagTimeInSeconds(0.2).build();
+            return PidParameters.builder().kp(1.5).kd(0.75).ki(0).lagTimeInSeconds(0.2).build();
         }
     };
 
