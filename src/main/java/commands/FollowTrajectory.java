77e68929b2851cbde168d8dc76a6b380eaf4d78b
Hoang Tung Dinh <tunghdinh@gmail.com>
2016-07-13 16:13:08 +0200

    cleaned up

--- src/main/java/commands/FollowTrajectory.java
+++ src/main/java/commands/FollowTrajectory.java
@@ -2,17 +2,11 @@ package commands;
 
 import com.google.common.base.Optional;
 import commands.schedulers.PeriodicTaskRunner;
-import control.PidController4d;
-import control.PidParameters;
 import control.Trajectory4d;
 import control.dto.DroneStateStamped;
-import control.dto.InertialFrameVelocity;
-import control.dto.Pose;
-import control.dto.Velocity;
 import control.localization.StateEstimator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import services.VelocityService;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
@@ -26,12 +20,7 @@ public final class FollowTrajectory implements Command {
     private static final Logger velocityLogger = LoggerFactory.getLogger(
             FollowTrajectory.class.getName() + ".velocitylogger");
 
-    private final VelocityService velocityService;
     private final StateEstimator stateEstimator;
-    private final PidParameters pidLinearXParameters;
-    private final PidParameters pidLinearYParameters;
-    private final PidParameters pidLinearZParameters;
-    private final PidParameters pidAngularZParameters;
     private final Trajectory4d trajectory4d;
     private final double durationInSeconds;
     private final double controlRateInSeconds;
@@ -40,12 +29,7 @@ public final class FollowTrajectory implements Command {
     private final VelocityController velocityController;
 
     private FollowTrajectory(Builder builder) {
-        velocityService = builder.velocityService;
         stateEstimator = builder.stateEstimator;
-        pidLinearXParameters = builder.pidLinearXParameters;
-        pidLinearYParameters = builder.pidLinearYParameters;
-        pidLinearZParameters = builder.pidLinearZParameters;
-        pidAngularZParameters = builder.pidAngularZParameters;
         trajectory4d = builder.trajectory4d;
         durationInSeconds = builder.durationInSeconds;
         controlRateInSeconds = builder.controlRateInSeconds;
@@ -53,13 +37,13 @@ public final class FollowTrajectory implements Command {
 
         final CreateVelocityControllerVisitor controllerVisitor = CreateVelocityControllerVisitor.builder()
                 .withTrajectory4d(trajectory4d)
-                .withPidLinearXParameters(pidLinearXParameters)
-                .withPidLinearYParameters(pidLinearYParameters)
-                .withPidLinearZParameters(pidLinearZParameters)
-                .withPidAngularZParameters(pidAngularZParameters)
+                .withPidLinearXParameters(builder.pidLinearXParameters)
+                .withPidLinearYParameters(builder.pidLinearYParameters)
+                .withPidLinearZParameters(builder.pidLinearZParameters)
+                .withPidAngularZParameters(builder.pidAngularZParameters)
                 .build();
 
-        velocityController = controllerVisitor.createVelocityController(velocityService);
+        velocityController = controllerVisitor.createVelocityController(builder.velocityService);
     }
 
     public static Builder builder() {
@@ -78,26 +62,15 @@ public final class FollowTrajectory implements Command {
     }
 
     private final class ComputeNextResponse implements Runnable {
-        private final PidController4d pidController4d;
         private final double startTimeInNanoSeconds;
         private final int stateLifeDurationInNumberOfControlLoops;
 
         private int counter = 0;
         private double lastTimeStamp = Double.MIN_VALUE;
-        private final InertialFrameVelocity zeroVelocity = Velocity.createZeroVelocity();
-        private final Pose zeroPose = Pose.createZeroPose();
 
         private static final double NANO_SECOND_TO_SECOND = 1.0E09;
 
         private ComputeNextResponse() {
-            this.pidController4d = PidController4d.builder()
-                    .linearXParameters(pidLinearXParameters)
-                    .linearYParameters(pidLinearYParameters)
-                    .linearZParameters(pidLinearZParameters)
-                    .angularZParameters(pidAngularZParameters)
-                    .trajectory4d(trajectory4d)
-                    .build();
-
             this.startTimeInNanoSeconds = System.nanoTime();
             this.stateLifeDurationInNumberOfControlLoops = (int) Math.ceil(
                     droneStateLifeDurationInSeconds / controlRateInSeconds);
