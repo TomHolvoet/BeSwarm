4c6615e432f0cc2bf8d63661f7d7f1da9196d562
Kristof Coninx <kristof.coninx@cs.kuleuven.be>
2016-08-22 22:45:11 +0200

    Fixed wiggle trajectory to specs.

--- src/main/java/applications/trajectory/WiggleTrajectory.java
+++ src/main/java/applications/trajectory/WiggleTrajectory.java
@@ -11,45 +11,36 @@ import control.FiniteTrajectory4d;
  * @author Kristof Coninx <kristof.coninx AT cs.kuleuven.be>
  */
 public class WiggleTrajectory extends BasicTrajectory implements FiniteTrajectory4d {
-    private static final double WIGGLE_VELOCITY = 2d;
+    private static final double WIGGLE_DISTANCE = 0.5d;
+    private static final double TIME_TO_REST_AT_ORIGIN = 0.001d;
     private final FiniteTrajectory4d target;
 
-    WiggleTrajectory(Point4D centerPoint, int wiggles, double distance) {
+    WiggleTrajectory(Point4D centerPoint, int wiggles, double timeToStayAtEdges) {
         Choreography.Builder builder = Choreography.builder();
         double orientation = centerPoint.getAngle();
         Point4D endRight =
                 Point4D.create(
-                        centerPoint.getX() + StrictMath.cos(orientation) * distance,
-                        centerPoint.getY() + StrictMath.sin(orientation) * distance,
+                        centerPoint.getX() + StrictMath.cos(orientation) * WIGGLE_DISTANCE,
+                        centerPoint.getY() + StrictMath.sin(orientation) * WIGGLE_DISTANCE,
                         centerPoint.getZ(),
                         orientation);
         Point4D endLeft =
                 Point4D.create(
-                        centerPoint.getX() - StrictMath.cos(orientation) * distance,
-                        centerPoint.getY() - StrictMath.sin(orientation) * distance,
+                        centerPoint.getX() - StrictMath.cos(orientation) * WIGGLE_DISTANCE,
+                        centerPoint.getY() - StrictMath.sin(orientation) * WIGGLE_DISTANCE,
                         centerPoint.getZ(),
                         orientation);
 
-        for (int i = 0; i < wiggles; i++) { //todo try hold + forTime
-            //      builder.withTrajectory(
-            //          new AbstractUncheckedStraightLineTrajectory4D(centerPoint, endRight,
-            // WIGGLE_VELOCITY));
-            //      builder.withTrajectory(
-            //          new AbstractUncheckedStraightLineTrajectory4D(endRight, endLeft,
-            // WIGGLE_VELOCITY));
-            //      builder.withTrajectory(
-            //          new AbstractUncheckedStraightLineTrajectory4D(endLeft, centerPoint,
-            // WIGGLE_VELOCITY));
-
+        for (int i = 0; i < wiggles; i++) {
             builder.withTrajectory(
                     Trajectories.newHoldPositionTrajectory(endRight))
-                    .forTime(distance / WIGGLE_VELOCITY);
+                    .forTime(timeToStayAtEdges);
             builder.withTrajectory(
                     Trajectories.newHoldPositionTrajectory(endLeft))
-                    .forTime(2 * distance / WIGGLE_VELOCITY);
+                    .forTime(2 * timeToStayAtEdges);
             builder.withTrajectory(
                     Trajectories.newHoldPositionTrajectory(centerPoint))
-                    .forTime(distance / WIGGLE_VELOCITY);
+                    .forTime(TIME_TO_REST_AT_ORIGIN);
         }
         target = builder.build();
     }
