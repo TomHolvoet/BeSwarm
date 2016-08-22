7ea435e0596ab9613ae65f6018ac968e8ee5794d
Kristof Coninx <kristof.coninx@cs.kuleuven.be>
2016-08-22 17:12:46 +0200

    Updated wiggle.

--- src/main/java/applications/trajectory/WiggleTrajectory.java
+++ src/main/java/applications/trajectory/WiggleTrajectory.java
@@ -11,58 +11,71 @@ import control.FiniteTrajectory4d;
  * @author Kristof Coninx <kristof.coninx AT cs.kuleuven.be>
  */
 public class WiggleTrajectory extends BasicTrajectory implements FiniteTrajectory4d {
-  private static final double WIGGLE_VELOCITY = 1d;
-  private final FiniteTrajectory4d target;
+    private static final double WIGGLE_VELOCITY = 2d;
+    private final FiniteTrajectory4d target;
 
-  WiggleTrajectory(Point4D centerPoint, int wiggles, double distance) {
-    Choreography.Builder builder = Choreography.builder();
-    double orientation = centerPoint.getAngle();
-    Point4D endRight =
-        Point4D.create(
-            centerPoint.getX() + StrictMath.cos(orientation) * distance,
-            centerPoint.getY() + StrictMath.sin(orientation) * distance,
-            centerPoint.getZ(),
-            orientation);
-    Point4D endLeft =
-        Point4D.create(
-            centerPoint.getX() - StrictMath.cos(orientation) * distance,
-            centerPoint.getY() - StrictMath.sin(orientation) * distance,
-            centerPoint.getZ(),
-            orientation);
+    WiggleTrajectory(Point4D centerPoint, int wiggles, double distance) {
+        Choreography.Builder builder = Choreography.builder();
+        double orientation = centerPoint.getAngle();
+        Point4D endRight =
+                Point4D.create(
+                        centerPoint.getX() + StrictMath.cos(orientation) * distance,
+                        centerPoint.getY() + StrictMath.sin(orientation) * distance,
+                        centerPoint.getZ(),
+                        orientation);
+        Point4D endLeft =
+                Point4D.create(
+                        centerPoint.getX() - StrictMath.cos(orientation) * distance,
+                        centerPoint.getY() - StrictMath.sin(orientation) * distance,
+                        centerPoint.getZ(),
+                        orientation);
 
-    for (int i = 0; i < wiggles; i++) {
-      builder.withTrajectory(
-          new AbstractUncheckedStraightLineTrajectory4D(centerPoint, endRight, WIGGLE_VELOCITY));
-      builder.withTrajectory(
-          new AbstractUncheckedStraightLineTrajectory4D(endRight, endLeft, WIGGLE_VELOCITY));
-      builder.withTrajectory(
-          new AbstractUncheckedStraightLineTrajectory4D(endLeft, centerPoint, WIGGLE_VELOCITY));
+        for (int i = 0; i < wiggles; i++) { //todo try hold + forTime
+            //      builder.withTrajectory(
+            //          new AbstractUncheckedStraightLineTrajectory4D(centerPoint, endRight,
+            // WIGGLE_VELOCITY));
+            //      builder.withTrajectory(
+            //          new AbstractUncheckedStraightLineTrajectory4D(endRight, endLeft,
+            // WIGGLE_VELOCITY));
+            //      builder.withTrajectory(
+            //          new AbstractUncheckedStraightLineTrajectory4D(endLeft, centerPoint,
+            // WIGGLE_VELOCITY));
+
+            builder.withTrajectory(
+                    Trajectories.newHoldPositionTrajectory(endRight))
+                    .forTime(distance / WIGGLE_VELOCITY);
+            builder.withTrajectory(
+                    Trajectories.newHoldPositionTrajectory(endLeft))
+                    .forTime(2 * distance / WIGGLE_VELOCITY);
+            builder.withTrajectory(
+                    Trajectories.newHoldPositionTrajectory(centerPoint))
+                    .forTime(distance / WIGGLE_VELOCITY);
+        }
+        target = builder.build();
     }
-    target = builder.build();
-  }
 
-  @Override
-  public double getTrajectoryDuration() {
-    return target.getTrajectoryDuration();
-  }
+    @Override
+    public double getTrajectoryDuration() {
+        return target.getTrajectoryDuration();
+    }
 
-  @Override
-  public double getDesiredPositionX(double timeInSeconds) {
-    return target.getDesiredPositionX(timeInSeconds);
-  }
+    @Override
+    public double getDesiredPositionX(double timeInSeconds) {
+        return target.getDesiredPositionX(timeInSeconds);
+    }
 
-  @Override
-  public double getDesiredPositionY(double timeInSeconds) {
-    return target.getDesiredPositionY(timeInSeconds);
-  }
+    @Override
+    public double getDesiredPositionY(double timeInSeconds) {
+        return target.getDesiredPositionY(timeInSeconds);
+    }
 
-  @Override
-  public double getDesiredPositionZ(double timeInSeconds) {
-    return target.getDesiredPositionZ(timeInSeconds);
-  }
+    @Override
+    public double getDesiredPositionZ(double timeInSeconds) {
+        return target.getDesiredPositionZ(timeInSeconds);
+    }
 
-  @Override
-  public double getDesiredAngleZ(double timeInSeconds) {
-    return target.getDesiredAngleZ(timeInSeconds);
-  }
+    @Override
+    public double getDesiredAngleZ(double timeInSeconds) {
+        return target.getDesiredAngleZ(timeInSeconds);
+    }
 }
