d6c5060d5f6e660d193ad5c7b06b403c80fbaaee
Hoang Tung Dinh <tunghdinh@gmail.com>
2016-09-27 12:28:26 +0200

    added StartTimeAnnouncer

---
+++ src/main/java/applications/timesync/StartTimeAnnouncer.java
@@ -0,0 +1,40 @@
+package applications.timesync;
+
+import org.ros.message.Time;
+import org.ros.namespace.GraphName;
+import org.ros.node.AbstractNodeMain;
+import org.ros.node.ConnectedNode;
+import org.ros.node.topic.Publisher;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+/** @author Hoang Tung Dinh */
+public final class StartTimeAnnouncer extends AbstractNodeMain {
+  @Override
+  public GraphName getDefaultNodeName() {
+    return GraphName.of("StartTimeAnnouncer");
+  }
+
+  @Override
+  public void onStart(final ConnectedNode connectedNode) {
+    final Time startTime = connectedNode.getCurrentTime();
+    final Publisher<std_msgs.Time> startTimePublisher =
+        connectedNode.newPublisher("start_time", std_msgs.Time._TYPE);
+
+    final std_msgs.Time timeMsgs = startTimePublisher.newMessage();
+    timeMsgs.setData(startTime);
+
+    Executors.newSingleThreadScheduledExecutor()
+        .scheduleAtFixedRate(
+            new Runnable() {
+              @Override
+              public void run() {
+                startTimePublisher.publish(timeMsgs);
+              }
+            },
+            0,
+            20,
+            TimeUnit.MILLISECONDS);
+  }
+}
