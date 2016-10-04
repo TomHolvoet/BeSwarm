7788f0d660d1edc5da4cda85f318ef91651ab1ff
Hoang Tung Dinh <tunghdinh@gmail.com>
2016-10-04 14:34:12 +0200

    added PoseOutdatedMonitor

---
+++ src/main/java/monitors/PoseOutdatedMonitor.java
@@ -0,0 +1,66 @@
+package monitors;
+
+import com.google.common.base.Optional;
+import control.dto.DroneStateStamped;
+import localization.StateEstimator;
+import time.TimeProvider;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicReference;
+
+/** @author Hoang Tung Dinh */
+public final class PoseOutdatedMonitor {
+  private final StateEstimator stateEstimator;
+  private final TimeProvider timeProvider;
+  private final double expiredTimeInSecs;
+
+  private final AtomicReference<PoseStatus> poseStatus = new AtomicReference<>(PoseStatus.OUTDATED);
+
+  private PoseOutdatedMonitor(
+      final StateEstimator stateEstimator,
+      final TimeProvider timeProvider,
+      final double expiredTimeInSecs) {
+    this.stateEstimator = stateEstimator;
+    this.timeProvider = timeProvider;
+    this.expiredTimeInSecs = expiredTimeInSecs;
+
+    Executors.newSingleThreadScheduledExecutor()
+        .scheduleAtFixedRate(new Monitor(), 0, 50, TimeUnit.MILLISECONDS);
+  }
+
+  public static PoseOutdatedMonitor create(
+      StateEstimator stateEstimator, TimeProvider timeProvider, double expiredTimeInSecs) {
+    return new PoseOutdatedMonitor(stateEstimator, timeProvider, expiredTimeInSecs);
+  }
+
+  public PoseStatus getPoseStatus() {
+    return poseStatus.get();
+  }
+
+  public enum PoseStatus {
+    VALID,
+    OUTDATED
+  }
+
+  private final class Monitor implements Runnable {
+
+    private Monitor() {}
+
+    @Override
+    public void run() {
+      final Optional<DroneStateStamped> currentState = stateEstimator.getCurrentState();
+
+      if (currentState.isPresent()) {
+        final double currentTime = timeProvider.getCurrentTimeSeconds();
+        if (currentTime - currentState.get().getTimeStampInSeconds() >= expiredTimeInSecs) {
+          poseStatus.set(PoseStatus.OUTDATED);
+        } else {
+          poseStatus.set(PoseStatus.VALID);
+        }
+      } else {
+        poseStatus.set(PoseStatus.OUTDATED);
+      }
+    }
+  }
+}
