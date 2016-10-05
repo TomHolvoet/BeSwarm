2271b87403bda3407dace6e69f274930e757d2d5
Hoang Tung Dinh <tunghdinh@gmail.com>
2016-10-05 15:26:11 +0200

    improved OfflineCollisionChecker

--- src/main/java/applications/offlinecheckers/OfflineCollisionChecker.java
+++ src/main/java/applications/offlinecheckers/OfflineCollisionChecker.java
@@ -1,10 +1,14 @@
 package applications.offlinecheckers;
 
+import com.google.common.base.MoreObjects;
 import control.FiniteTrajectory4d;
 import control.dto.Pose;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
 
 /** @author Hoang Tung Dinh */
 public final class OfflineCollisionChecker {
@@ -23,8 +27,9 @@ public final class OfflineCollisionChecker {
     return new OfflineCollisionChecker(minimumDistance, trajectories);
   }
 
-  public String checkCollision() {
-    final LinkedList<FiniteTrajectory4d> trajectoryList = new LinkedList<>(trajectories);
+  public List<Collision> checkCollision() {
+    final Queue<FiniteTrajectory4d> trajectoryList = new LinkedList<>(trajectories);
+    final List<Collision> collisionList = new ArrayList<>();
 
     while (!trajectoryList.isEmpty()) {
       final FiniteTrajectory4d trajectory = trajectoryList.remove();
@@ -37,11 +42,7 @@ public final class OfflineCollisionChecker {
           final Pose secondPose = Pose.createFromTrajectory(otherTrajectory, t);
           final double distance = Pose.computeEucllideanDistance(firstPose, secondPose);
           if (distance < minimumDistance) {
-            return String.format(
-                "There is a collision at %f between trajectory %s and trajectory %s",
-                t,
-                trajectory,
-                otherTrajectory);
+            collisionList.add(Collision.create(trajectory, otherTrajectory, t));
           } else {
             t += DELTA_TIME;
           }
@@ -49,6 +50,37 @@ public final class OfflineCollisionChecker {
       }
     }
 
-    return "Yay! There is no collision.";
+    return collisionList;
+  }
+
+  public static class Collision {
+    private final FiniteTrajectory4d firstTrajectory;
+    private final FiniteTrajectory4d secondTrajectory;
+    private final double collisionTimeInSecs;
+
+    private Collision(
+        FiniteTrajectory4d firstTrajectory,
+        FiniteTrajectory4d secondTrajectory,
+        double collisionTimeInSecs) {
+      this.firstTrajectory = firstTrajectory;
+      this.secondTrajectory = secondTrajectory;
+      this.collisionTimeInSecs = collisionTimeInSecs;
+    }
+
+    public static Collision create(
+        FiniteTrajectory4d firstTrajectory,
+        FiniteTrajectory4d secondTrajectory,
+        double collisionTimeInSecs) {
+      return new Collision(firstTrajectory, secondTrajectory, collisionTimeInSecs);
+    }
+
+    @Override
+    public String toString() {
+      return MoreObjects.toStringHelper(this)
+          .add("firstTrajectory", firstTrajectory)
+          .add("secondTrajectory", secondTrajectory)
+          .add("collisionTimeInSecs", collisionTimeInSecs)
+          .toString();
+    }
   }
 }
